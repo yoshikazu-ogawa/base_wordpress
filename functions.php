163cdb9aff1d10619e564b1b83fbfc8aa0a2f5e6
yoshikazuogawa <yoshikazu.ogawa@gmail.com>
2014-07-06 00:51:22 +0900

    function.php にセルフピンバック禁止を追加

--- functions.php
+++ functions.php
@@ -27,6 +27,15 @@ function remove_admin_menu_links() {
      //remove_menu_page('options-general.php'); // 設定
 }
 
+// セルフピンバック禁止
+function no_self_ping( &$links ) {
+$home = get_option( 'home' );
+foreach ( $links as $l => $link )
+if ( 0 === strpos( $link, $home ) )
+unset($links[$l]);
+}
+add_action( 'pre_ping', 'no_self_ping' );
+
 //パンくず
 function breadcrumb(){
 	global $post;
