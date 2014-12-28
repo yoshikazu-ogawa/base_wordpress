3fa2822277d308e9fa678761302f463a4b60af0e
yogawa <yoshikazu.ogawa@gmail.com>
2014-12-29 08:52:37 +0900

    ループをget_postsに変更

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <div id="content">
 
 	<main id="main" role="main">
-
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'content', get_post_format() ); ?>
-
-		<?php endwhile; endif;?>
-
+		<?php
+		$postslist = get_posts('post_type=post&numberposts=5');
+		foreach ($postslist as $post) : setup_postdata($post);?> 
+		 <article>
+			<time datetime="<?php echo get_the_date('c');?>"><?php echo get_the_date();?></time>
+			<h1><a href="<?php the_permalink();?>"><?php the_title();?></a></h1>
+		 </article>
+		<?php endforeach; ?>
 	</main>
 
 	<?php get_sidebar(); ?>
